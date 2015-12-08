f979d8c10f2b801d48a111f5a49efdb8550376ad
hugooconnor <hugo.oconnor@gmail.com>
2015-12-08 17:05:12 +1100

    added modifier, added cost estimates to each function

--- contracts/id.sol
+++ contracts/id.sol
@@ -30,50 +30,44 @@ contract Identity {
 	//however, for privacy, on chain event logging is preferrable
 	Access[] accessEvents;
 
-	//constructor
+	//constructor -- cost 4.08341567825 USD
 	function Identity() { 
 		owner = msg.sender;
 		logAccess("new identity", "genesis", msg.sender);
 		}
 
-	//delete an identity and its contents
-	function kill() { if (msg.sender == owner) suicide(owner); }
+	//onlyowner function modifier
+	modifier onlyowner { if (msg.sender == owner) _ }
 
-	//log access event
+	//delete an identity and its contents -- cost 0.0007134115 USD
+	function kill() onlyowner { suicide(owner); }
+
+	//log access event -- cost 0.11263427475 USD
 	function logAccess (string accessType, string hash, address accessor) {
 		accessEvents.push(Access(accessType, hash, accessor, now));
 	}
 
-	//create new identity attribute data
-	function assert (string index, string hash, bytes32 key, bool isPublic) 
-	returns (bool success){
-		//anyone with access to the contract, can make assertions
-		//check if attribute has already been asserted
-		//only identity owner can overwrite attributes
-		if(bytes(indexRegister[index]).length == 0 || msg.sender == owner){
-			indexRegister[index] = hash;
-			keyRegister[hash] = key;
-			publicRegister[hash] = isPublic;
-			permissionsRegister[msg.sender][hash] = Condition(true, 0);
-			attributeList.push(index);
-			logAccess("new assertion", hash, msg.sender);
-			return true;
-		}
-		return false;
+	//create new identity attribute data -- cost 0.56110996925 USD
+	function assert(string index, string hash, bytes32 key, bool isPublic) onlyowner {
+		//only the identity owner can make assertions about their identity
+		indexRegister[index] = hash;
+		keyRegister[hash] = key;
+		publicRegister[hash] = isPublic;
+		permissionsRegister[msg.sender][hash] = Condition(true, 0);
+		attributeList.push(index);
+		logAccess("new assertion", hash, msg.sender);
 	}
 	
-	//create new attribute meta-data
-	function attest (string hash) returns (bool success) {
+	//create new attribute meta-data -- cost 0.100721091 USD
+	function attest (string hash) {
 		//check if public or attestor has permission to attest
 		if(publicRegister[hash] || permissionsRegister[msg.sender][hash].access){
 			attestationRegister[hash].push(msg.sender);
 			logAccess("new attestation", hash, msg.sender);
-			return true;
 		}
-		return false;
 	}
 
-	//get the assertion of a particular index value
+	//get the assertion of a particular index value -- cost 0.1026169525 USD
 	function getAssertion (string index) returns (string _ipfsHash, bytes32 key) {
 		//get the relevant ipfsHash
 		string ipfsHash = indexRegister[index];
@@ -87,7 +81,7 @@ contract Identity {
 		}
 	}
 
-	//return the attestations of a particular assertion
+	//return the attestations of a particular assertion -- cost 0.241708546 USD
 	function getAttestations (string hash) returns (address[] attestations){
 		//only people who know the hash, can view the attestations
 		//or do we want anyone to be able to see this?
@@ -95,43 +89,34 @@ contract Identity {
 		return attestationRegister[hash];
 	}
 
-	//set permissions for inbound assertion access
-	function setPermission (string index, address permittedUser, uint cost, bool permitted)
-	 returns (bool success) {
+	//set permissions for inbound assertion access -- 0.241558769 USD
+	function setPermission (string index, address permittedUser, uint cost, bool permitted) onlyowner{
 		//only the owner can add Permissions
-		if(msg.sender == owner){
-			permissionsRegister[permittedUser][index] = Condition(permitted, cost);
-			logAccess("set permission", index, msg.sender);
-			return true;
-		}
-		return false;
+		permissionsRegister[permittedUser][index] = Condition(permitted, cost);
+		logAccess("set permission", index, msg.sender);
 	}
 
 	//return the number of attributes of an identity
-	function getAttributesLength () returns (uint length){
-		if(msg.sender == owner){
-			return attributeList.length;
-		}
-		return 0;
+	// cost 0.00108194175 USD
+	function getAttributesLength () onlyowner returns (uint length) {
+		return attributeList.length;
 	}
 
 	//return attribute index of an identity (will need to run a loop on the client side)
+	// cost 0.050703456 USD
 	function getAttribute (uint index) returns (string attribute){
 		if(msg.sender == owner && attributeList.length < index){
 			return attributeList[index];
 		}
-		return "";
 	}
 
 	//get length of accessEvents
-	function getAccessLength () returns (uint length){
-		if(msg.sender == owner){
-			return accessEvents.length;
-		}
-		return 0;
+	// cost 0.00103858525 USD
+	function getAccessLength () onlyowner returns (uint length) {
+		return accessEvents.length;
 	}
 
-	//return an access event from the access log
+	//return an access event from the access log -- cost 0.10075853525 USD
 	function getAccessEvent (uint index) returns (string a, string h, address o, uint t){
 		if(msg.sender == owner && accessEvents.length < index){
 			Access e = accessEvents[index];
@@ -141,10 +126,9 @@ contract Identity {
 
 	//clear out the balance of the contract
 	//this is where the ID service provider would add their conditions
-	function sweepFunds() {
-		if(msg.sender == owner){
-			owner.send(this.balance);
-		}
+	//cost 0.00133419775 USD
+	function sweepFunds() onlyowner{
+		owner.send(this.balance);
 	}
 
 }
