b58a803921a483d40cc442cde82ceb568df7e006
Niels Draaisma <niels@draaisma.email>
2015-12-16 11:38:45 +1100

    Initial assertion work

---
+++ app/app/scripts/controllers/assert.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:AssertCtrl
+ * @description
+ * # AssertCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('AssertCtrl', function ($log, Identity) {
+      var self = this;
+
+      self.assertionType = 'name';
+      self.assertionValue = undefined;
+
+      self.assert = function() {
+        $log.info("Asserting "+self.assertionType+" : "+self.assertionValue);
+        Identity.generateAssertion(self.assertionType, self.assertionValue);
+      };
+
+  });
---
+++ app/test/spec/controllers/assert.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: AssertCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('angularApp'));
+
+  var AssertCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    AssertCtrl = $controller('AssertCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(AssertCtrl.awesomeThings.length).toBe(3);
+  });
+});
