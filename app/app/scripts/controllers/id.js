9f078a9f82a34313c34183b6427cea52a2cfaeb4
Niels Draaisma <niels@draaisma.email>
2015-12-22 15:56:47 +1100

    Layout tidyup, fixed batching of assertions

--- app/app/scripts/controllers/id.js
+++ app/app/scripts/controllers/id.js
@@ -11,37 +11,53 @@ angular.module('angularApp')
   .controller('IdentityCtrl', function ($log, $rootScope, $scope, $state, IdentityContract, Identity, Notification) {
       var self = this;
 
-      self.assertionType = $rootScope.assertionTypes[0];
-      self.assertionValue = '';
-
-      // Store request params
-      //$log.info($state.params);
-      self.request = {
-        requestee: $state.params.requestee,
-        assertionTypes: $state.params.assertionTypes,
-      };
+      self.assertions = {};
+      self.changedAssertions = {};
 
       self.assert = function() {
-        $log.info('Asserting '+self.assertionType.label+' : '+self.assertionValue);
-        Notification.primary('Asserting '+self.assertionType.label+' : '+self.assertionValue);
-        IdentityContract.assert($rootScope.selectedIdentity, self.assertionType.id, self.assertionValue);
+        $log.info('Storing changed assertions ', self.changedKeys());
+        var assertions = self.changedKeys().map(function(assertionId){
+          return {
+            assertionId: assertionId,
+            value: self.assertions[assertionId]
+          }});
+        IdentityContract.assert($rootScope.selectedIdentity, assertions);
       };
 
-      self.read = function() {
-        $log.info('Reading '+self.assertionType.label+' from contract');
-        IdentityContract.readAssertion($rootScope.selectedIdentity, $rootScope.selectedIdentity.contractAddress, self.assertionType.id)
+      self.read = function(assertionType) {
+        $log.info('Reading '+assertionType.label+' from contract');
+        IdentityContract.readAssertion($rootScope.selectedIdentity, $rootScope.selectedIdentity.contractAddress, assertionType.id)
           .then(function(decryptedAssertion){
               Notification.success('Got assertion value : '+decryptedAssertion);
-              $log.info('Got assertion value : '+decryptedAssertion);
-              self.assertionValue = decryptedAssertion;
+              self.assertions[assertionType.id] = decryptedAssertion;
+              delete self.changedAssertions[assertionType.id];
               $scope.$apply();
           });
-        Notification.primary('Reading '+self.assertionType.label+' from contract');
+        Notification.primary('Reading '+assertionType.label+' from contract');
       };
 
-      self.grantRequest = function() {
-        var grantee = Identity.get(self.request.requestee);
-        IdentityContract.grant($rootScope.selectedIdentity, grantee, self.request.assertionTypes);
+      self.init = function(){
+        $rootScope.assertionTypes.map(self.read);
+      };
+      self.init();
+
+      self.changedKeys = function(){
+        var keys = [];
+        for(var k in self.changedAssertions) keys.push(k);
+        return keys;
+      };
+
+      self.hasChangedKeys = function(){
+        return self.changedKeys().length > 0;
+      };
+
+      self.changed = function(assertionType) {
+        self.changedAssertions[assertionType.id] = true;
+        // $log.debug("Currently changed assetions", self.changedAssertions);
+      };
+
+      self.isChanged = function(assertionType){
+        return  self.changedKeys().lastIndexOf(''+assertionType.id) > -1;
       };
 
 
