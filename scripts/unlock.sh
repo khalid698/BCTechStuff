20bb076e093bcebdf7609da6e427536b2a12d2ab
Niels Draaisma <niels@draaisma.email>
2016-01-05 10:30:28 +1100

    Removed deprecated scripts and documentation, added reset script

--- scripts/unlock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"0x3e0d0e42ce28b5ae471dfc4ade385a4bf39dc8a9\",\"12345\"]}" http://localhost:8545 2>/dev/null | jq .result
